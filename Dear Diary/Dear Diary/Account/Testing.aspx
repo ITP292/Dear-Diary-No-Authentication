5c2e7c0d5d15f37b4a1823657af43c19425bc020
Joanne <Joanne@DESKTOP-LDPOB8G>
2016-12-24 12:00:43 +0800

    added Testing page - for my phone number validation to be added into Registration page

---
+++ Dear Diary/Dear Diary/Account/Testing.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Testing.aspx.cs" Inherits="Dear_Diary.Account.Testing" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <br />
+    <asp:Label ID="Label1" runat="server" Text="Phone  "></asp:Label>
+
+    <asp:TextBox ID="TextBox1" runat="server" TextMode="Phone"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Button ID="Button1" runat="server" Text="OK" OnClick="Button1_Click" />
+
+    <br />
+    <br />
+    <asp:Label ID="Label2" runat="server"></asp:Label>
+
+</asp:Content>
---
+++ Dear Diary/Dear Diary/Account/Testing.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Dear_Diary.Account
+{
+    public partial class Testing : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string phoneNo = TextBox1.Text;
+            string firstDigit = phoneNo.Substring(0, 1);
+            bool result = IsAllDigits(phoneNo);
+
+            
+            if (phoneNo.Length == 8 && (firstDigit == "9" || firstDigit == "8") && result == true)
+            {
+                Label2.Text = "Good phone number";
+            }
+            else
+            {
+                //if phone number length is not 8 and does not start with 8 or 9
+                
+                //if phone number length is not 8, say this
+                if (phoneNo.Length != 8 && result == true)
+                {
+                    Label2.Text = "Phone Number must be 8 digits.";
+                }
+                //if phone number length is 8, see if digit is 8 or 9. 
+                else if (firstDigit != "9" || firstDigit != "8" || result == false)
+                {
+                    Label2.Text = "Invalid phone number";
+                }
+            }
+        }
+        
+        //to check if phone string is fully digits, no alphabets or special characters.
+        bool IsAllDigits(string s)
+        {
+            foreach (char c in s)
+            {
+                if (!char.IsDigit(c))
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
