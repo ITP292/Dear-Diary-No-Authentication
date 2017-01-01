1f6d0ffedb12094ed693ad1a59aa5df0bc915efa
Joanne <Joanne@DESKTOP-LDPOB8G>
2017-01-01 22:25:20 +0800

    ert

--- Dear Diary/Dear Diary/Account/Testing4.aspx
+++ Dear Diary/Dear Diary/Account/Testing4.aspx
@@ -1,3 +1,13 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Testing4.aspx.cs" Inherits="Dear_Diary.Account.Testing4" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <br />
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Start" />
+    <br />
+    <br />
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+    <asp:Timer ID="Timer1" runat="server" Interval ="1000">
+    </asp:Timer>
+
 </asp:Content>
--- Dear Diary/Dear Diary/Account/Testing4.aspx.cs
+++ Dear Diary/Dear Diary/Account/Testing4.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Timers;
 
 namespace Dear_Diary.Account
 {
@@ -11,6 +12,28 @@ namespace Dear_Diary.Account
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Label1.Visible = false;
+        }
+        
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            System.Timers.Timer t1 = new System.Timers.Timer();
+            t1.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            t1.Interval = 1000; //1 second
+            t1.Enabled = true;
+            t1.Start();
+
+        }
+        protected void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            //Label1.Text = "Success, end of timer";
+            Label1.Visible = true;
+        }
+
+        protected void Tick (object source, ElapsedEventArgs e)
+        {
+            Label1.Visible = true;
 
         }
     }
