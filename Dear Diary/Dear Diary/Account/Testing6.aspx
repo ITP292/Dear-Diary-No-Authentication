c96acb3f8624a2144e854f5fde5f5928facf5057
Joanne <Joanne@DESKTOP-LDPOB8G>
2017-01-03 22:10:53 +0800

    asd

---
+++ Dear Diary/Dear Diary/Account/Testing6.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Testing6.aspx.cs" Inherits="Dear_Diary.Account.Testing6" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <br />
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+    <br />
+    <asp:Label ID="Label1" runat="server" Text="Counter"></asp:Label>
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Message"></asp:Label>
+</asp:Content>
---
+++ Dear Diary/Dear Diary/Account/Testing6.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Dear_Diary.Account
+{
+    public partial class Testing6 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        Label1.Text = count.ToString();
+
+        }
+
+        int count = 0;
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (count >= 0 && count <5)
+            {
+                count++;
+                Label1.Text = count.ToString();
+            }
+            else if (count == 5)
+            {
+                Label1.Text = count.ToString();
+                Label2.Text = "Counter has reached 5";
+
+            }
+        }
+    }
+}
