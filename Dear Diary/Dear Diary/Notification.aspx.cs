2819f9422bea1734be12aef711bfd9461eeabc78
Voltz <secretemailaddress01@gmail.com>
2017-01-15 15:58:57 +0800

    modified notification to handle errors gracefully

--- Dear Diary/Dear Diary/Notification.aspx.cs
+++ Dear Diary/Dear Diary/Notification.aspx.cs
@@ -13,8 +13,6 @@ namespace Dear_Diary
 {
     public partial class Notification : System.Web.UI.Page
     {
-        //sfList stores related notification information about friends
-        private ArrayList sfList;
         private ArrayList cList = new ArrayList();
         private ArrayList pList = new ArrayList();
 
@@ -34,13 +32,14 @@ namespace Dear_Diary
         {
             //Remove existing list on the page and replace it with the new one
             tabs.Controls.Clear();
-            foreach(var item in s)
+            foreach (var item in s)
             {
                 HtmlGenericControl li = new HtmlGenericControl("li");
                 tabs.Controls.Add(li);
                 HtmlGenericControl anchor = new HtmlGenericControl("a");
                 anchor.Attributes.Add("href", "#");
-                anchor.InnerText = item.ToString();
+                //anchor.InnerText = item.ToString();
+                anchor.InnerText = Server.HtmlEncode(item.ToString());
                 li.Controls.Add(anchor);
             }
             //Things that need to be solved, count the number of notifications pulled from the database and use that number
@@ -49,30 +48,38 @@ namespace Dear_Diary
 
         protected ArrayList retrieveFriends()
         {
-            //fList stores information retrieved from Friends table
-            ArrayList fList = new ArrayList();
-            using (SqlConnection myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
+            try
             {
-                String User2_Email;
-                //String User1_Email = Session["email"].ToString();
-                String User1_Email = "lrh@gmail.com";
-                String query = "SELECT * FROM Friendship WHERE Seen = @seen AND User1_Email = @user1email";
-
-                SqlCommand myCommand = new SqlCommand(query, myConnection);
-                myConnection.Open();
-                myCommand.CommandType = CommandType.Text;
-                myCommand.Parameters.AddWithValue("@seen", "false");
-                myCommand.Parameters.AddWithValue("@user1email", User1_Email);
-
-                SqlDataReader reader = myCommand.ExecuteReader();
-
-                if (reader.Read())
+                //fList stores information retrieved from Friends table
+                ArrayList fList = new ArrayList();
+                using (SqlConnection myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
                 {
-                    User2_Email = reader["User2_Email"].ToString();
-                    fList.Add(User2_Email);
+                    String User2_Email;
+                    //String User1_Email = Session["email"].ToString();
+                    String User1_Email = "lrh@gmail.com";
+                    String query = "SELECT * FROM Friendship WHERE Seen = @seen AND User1_Email = @user1email";
+
+                    SqlCommand myCommand = new SqlCommand(query, myConnection);
+                    myConnection.Open();
+                    myCommand.CommandType = CommandType.Text;
+                    myCommand.Parameters.AddWithValue("@seen", "false");
+                    myCommand.Parameters.AddWithValue("@user1email", User1_Email);
+
+                    SqlDataReader reader = myCommand.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        User2_Email = reader["User2_Email"].ToString();
+                        fList.Add(User2_Email + " wants to add you as a friend!");
+                    }
+                    return fList;
                 }
             }
-                return fList;
+            catch (Exception)
+            {
+                Label3.Text = "Database Error";
+            }
+            return null;
         }
     }
 }
