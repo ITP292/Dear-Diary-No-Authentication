5fbc3822f96bfe8bce588a969660969998aea104
Joanne <Joanne@DESKTOP-LDPOB8G>
2017-01-20 00:23:18 +0800

    ggg

--- Dear Diary/Dear Diary/Account/Login.aspx.cs
+++ Dear Diary/Dear Diary/Account/Login.aspx.cs
@@ -177,32 +177,6 @@ namespace Dear_Diary.Account
                 Label8.Text = "";
             }
         }
-
-        //protected void Timer2_Tick(object sender, EventArgs e)
-        //{
-        //    timeCounter1++;
-        //    if (timeCounter1 >= 1)
-        //    {
-        //        SqlConnection myConnection;
-        //        using (myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
-        //        {
-        //            string inputemail = TextBox1.Text;
-        //            string randomNo = null;
-        //            //set randomNo to null once time is up
-        //            myConnection.Open();
-        //            string query1 = "UPDATE [dbo].[User] SET [randomNo] = @randomNo WHERE [Email_Address] = @inputemail";
-        //            SqlCommand myCommand1 = new SqlCommand(query1, myConnection);
-        //            myCommand1.CommandType = CommandType.Text;
-        //            myCommand1.Parameters.AddWithValue("@inputemail", inputemail);
-        //            myCommand1.Parameters.AddWithValue("@randomNo", randomNo);
-        //            myCommand1.ExecuteNonQuery();
-        //            myConnection.Close();
-        //        }
-        //            Timer2.Enabled = false;
-        //    }
-        //}
-
-        //generate otp code method
         private string GenerateRandomOTP(int iOTPLength, string[] saAllowedCharacters)
 
         {
