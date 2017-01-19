242b4f57b8b8825791d6a7139254628b1c9b5b80
Ryan <ryanchew96@gmail.com>
2017-01-19 18:02:43 +0800

    Upload logic

--- Dear Diary/Dear Diary/Profile/UploadPicture.aspx
+++ Dear Diary/Dear Diary/Profile/UploadPicture.aspx
@@ -1,3 +1,8 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="UploadPicture.aspx.cs" Inherits="Dear_Diary.Profile.WebForm3" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:FileUpload ID="FileUpload1" runat="server" />
+    <br />
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Upload" />
+    <br />
+    <asp:Image ID="Image1" runat="server" Height="118px" Width="104px" />
 </asp:Content>
--- Dear Diary/Dear Diary/Profile/UploadPicture.aspx.cs
+++ Dear Diary/Dear Diary/Profile/UploadPicture.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +14,30 @@ namespace Dear_Diary.Profile
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string fileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+                FileUpload1.PostedFile.SaveAs(Server.MapPath("~/Pictures/") + fileName);
+                Session["imagePath"] = "~/Pictures/" + fileName;
+                Image1.ImageUrl = "~/Pictures/" + fileName;
+                Image1.Visible = true;
+            }
+
+            using (SqlConnection myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
+            {
+                myConnection.Open();
+
+                string query = "INSERT INTO [Test] VALUES (@picture)";
+                SqlCommand myCommand = new SqlCommand(query, myConnection);
+
+                myCommand.Parameters.AddWithValue("@picture", Session["imagePath"].ToString());
+
+                myCommand.ExecuteNonQuery();
+            }
         }
     }
 }
