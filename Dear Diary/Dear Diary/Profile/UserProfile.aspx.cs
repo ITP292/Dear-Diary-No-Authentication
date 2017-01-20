58d3b955b19bcdbbf0a3218f40a6e86831311b49
Chu Angie <isadlife@gmail.com>
2017-01-20 11:18:54 +0800

    solved userprofile

--- Dear Diary/Dear Diary/Profile/UserProfile.aspx.cs
+++ Dear Diary/Dear Diary/Profile/UserProfile.aspx.cs
@@ -30,8 +30,8 @@ namespace Dear_Diary.Profile
                 String dbFName;
                 String dbLName;
                 String query = "SELECT [FName], [LName], [Email_Address], [displayPic] FROM [User] WHERE [Email_Address] = @email";
-
                 SqlCommand myCommand = new SqlCommand(query, myConnection);
+
                 myConnection.Open();
                 myCommand.CommandType = CommandType.Text;
                 myCommand.Parameters.AddWithValue("@email", email);
@@ -48,12 +48,69 @@ namespace Dear_Diary.Profile
                 }
 
                 Image1.ImageUrl = dbProfilePic;
-                userName.Text =  Name;
+                userName.Text = Name;
                 userEmail.Text = "(" + dbEmail + ")";
-                
-            }
-        }
 
+                myConnection.Close();
+
+
+                //{
+                    //String email2 = Session["email"].ToString();
+
+                    String email2 = "stupid@idiot.com";
+                    String dbUser2Email;
+
+                    String query2 = "SELECT [User2_Email] FROM [Friendship] WHERE [User1_Email] = @email";
+                    SqlCommand myCommand2 = new SqlCommand(query2, myConnection);
+                    myConnection.Open();
+                    myCommand2.CommandType = CommandType.Text;
+                    myCommand2.Parameters.AddWithValue("@email", email2);
+
+                    SqlDataReader reader2 = myCommand2.ExecuteReader();
+
+                    if (reader2.Read())
+                    {
+                        dbUser2Email = reader2["User2_Email"].ToString();
+
+
+                    }
+                    myConnection.Close();
+
+                    //{
+                        //String email2 = Session["email"].ToString();
+
+                        String permission = "";
+                        String dbPostText = "";
+
+                        String query3 = "SELECT [Post_Text] FROM [Post], [Permission_Status] WHERE [Author_Email] = @email";
+                        //AND[Permission] = @permission
+                        SqlCommand myCommand3 = new SqlCommand(query3, myConnection);
+
+                        myConnection.Open();
+                        myCommand3.CommandType = CommandType.Text;
+                        myCommand3.Parameters.AddWithValue("@email", email2);
+                        //myCommand3.Parameters.AddWithValue("@permission", "public");
+
+                        SqlDataReader reader3 = myCommand3.ExecuteReader();
+
+                        if (reader3.Read())
+                        {
+                            permission = reader3["Permission_Status"].ToString();
+                            dbPostText = reader3["Post_Text"].ToString();
+                            if (permission.Equals("public"))
+                            {
+                                TextBox1.Text = dbPostText;
+                            }
+                            else if (!permission.Equals("public"))
+                            {
+                                Label1.Text = "No posts available";
+                            }
+                        }
+
+                        myConnection.Close();
+                    }
+                //}
+            }
         protected void noOfPosts_Click(object sender, ImageClickEventArgs e)
         {
             Response.Redirect("/NewEntry/PostEntryList.aspx");
