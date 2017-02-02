6f5e1884b00ef34206545fbee30bc085f7d87f3e
Chu Angie <155105P@mymail.nyp.edu.sg>
2017-02-02 19:06:55 +0800

    Done a newsfeed page, but still haven't added friend's posts.

---
+++ Dear Diary/Dear Diary/Profile/NewsFeed.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="NewsFeed.aspx.cs" Inherits="Dear_Diary.Profile.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <p>
+        <br />
+        <asp:Repeater ID="Repeater1" runat="server" DataSourceID="SqlDataSource1" OnItemCommand="Repeater1_ItemCommand">
+            <ItemTemplate>
+                <a class="draft-post-link" href="/NewEntry/PostDetails.aspx?Post_Id=<%# Eval("Post_Id") %>">
+                    <div class="main_dtaft">
+                        <div class="img_draft left">
+                            <asp:Image ImageUrl='<%# Eval("Picture") %>' ID="img" runat="server" />
+                        </div>
+                        <div class="draft-content right">
+                            <p>
+                                <asp:Label ID="lblpostEntrytext" runat="server" Text='<%# Eval("Post_Text") %>' />
+                            </p>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </a>
+                <div class="post-footer">
+                    Posted on :
+                        <asp:Label ID="lblPostEntryOn" runat="server" Text='<%# Eval("Date_Added","{0:dd MMM yyyy}") %>' />
+                    <asp:LinkButton CssClass="draft-post-delete" CommandName="Delete" CommandArgument='<%# Eval("Post_Id") %>' Text="Delete Post" ID="lnkPostDelete" runat="server" />
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:localdbConnectionString1 %>" SelectCommand="SELECT * FROM [Post] WHERE ([Permission_Status] = @Permission_Status)">
+            <SelectParameters>
+                <asp:Parameter DefaultValue="Public" Name="Permission_Status" Type="String" />
+            </SelectParameters>
+    </asp:SqlDataSource>
+    </p>
+
+</asp:Content>
---
+++ Dear Diary/Dear Diary/Profile/NewsFeed.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Dear_Diary.Security_API;
+
+namespace Dear_Diary.Profile
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var loginEmail = Session["email"] != null ? Session["email"].ToString() : "stupid@idiot.com";
+            
+        }
+
+        // to get post details
+        public DataTable GetPostDetails(int IsPosted, string loginEmail)
+        {
+            String permission = "Public";
+            DataTable dt = new DataTable();
+            String query = "SELECT * FROM [Post] WHERE [IsPostEntry] = " + IsPosted + " and Author_Email = '" + loginEmail + "' AND Permission_Status = @Permission";
+
+            SqlConnection myConnection;
+            using (myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
+            {
+                String encrypted_post;
+                String plain_post;
+
+                SqlCommand myCommand = new SqlCommand(query, myConnection);
+                myConnection.Open();
+                myCommand.CommandType = CommandType.Text;
+                myCommand.Parameters.AddWithValue("@Permission", permission);
+                SqlDataAdapter da = new SqlDataAdapter(myCommand);
+                da.Fill(dt);
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    encrypted_post = row[4].ToString();
+                    plain_post = AES.Decrypt(encrypted_post);
+                    row.SetField(4, plain_post);
+                }
+
+                return dt;
+            }
+        }
+
+        protected void Repeater1_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            if (e.CommandName == "Delete")
+            {
+                var id = e.CommandArgument.ToString();
+                string query = "Delete FROM [Comment] WHERE [Post_Id] = " + id;
+                var loginEmail = Session["email"] != null ? Session["email"].ToString() : "test123@gmail.com";
+
+                SqlConnection myConnection;
+                using (myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
+                {
+                    SqlCommand myCommand = new SqlCommand(query, myConnection);
+                    myConnection.Open();
+                    myCommand.CommandType = CommandType.Text;
+                    myCommand.ExecuteNonQuery();
+                }
+
+                string query1 = "Delete FROM [Post] WHERE [Post_Id] = " + id;
+
+                SqlConnection myConnection1;
+                using (myConnection1 = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
+                {
+                    SqlCommand myCommand1 = new SqlCommand(query1, myConnection1);
+                    myConnection1.Open();
+                    myCommand1.CommandType = CommandType.Text;
+                    myCommand1.ExecuteNonQuery();
+                }
+
+                Repeater1.DataSource = GetPostDetails(1, loginEmail);
+                Repeater1.DataBind();
+
+                ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Post Entry deleted successfully.');", true);
+            }
+        }
+    }
+}
