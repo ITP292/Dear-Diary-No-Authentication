d79da66d451b76a4f5e108f6a8af1121d2bbf5b4
Ryan <ryanchew96@gmail.com>
2017-02-02 21:53:47 +0800

    Someone treating me teh c beng

--- Dear Diary/Dear Diary/Profile/NewsFeed.aspx.cs
+++ Dear Diary/Dear Diary/Profile/NewsFeed.aspx.cs
@@ -15,7 +15,8 @@ namespace Dear_Diary.Profile
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            var loginEmail = Session["email"] != null ? Session["email"].ToString() : "stupid@idiot.com";
+            //var loginEmail = Session["email"] != null ? Session["email"].ToString() : "stupid@idiot.com";
+            string loginEmail = "stupid@idiot.com";
             Repeater1.DataSource = GetPostDetails(1, loginEmail);
             Repeater1.DataBind();
         }
@@ -23,20 +24,39 @@ namespace Dear_Diary.Profile
         // to get post details
         public DataTable GetPostDetails(int IsPosted, string loginEmail)
         {
-            String permission = "Public";
-            DataTable dt = new DataTable();
-            String query = "SELECT * FROM [Post] WHERE [IsPostEntry] = " + IsPosted + " AND [Author_Email] = '" + loginEmail + "' ";
+            String FriendEmail = "";
 
-            SqlConnection myConnection;
-            using (myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
+            using (SqlConnection myConnection1 = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
+            {
+                String query1 = "SELECT * FROM [Friendship] Where [User1_Email] = @email";
+
+                SqlCommand myCommand1 = new SqlCommand(query1, myConnection1);
+                myConnection1.Open();
+                myCommand1.CommandType = CommandType.Text;
+                myCommand1.Parameters.AddWithValue("@email", loginEmail);
+
+                SqlDataReader reader = myCommand1.ExecuteReader();
+
+                if (reader.Read())
+                {
+                   FriendEmail = reader["User2_Email"].ToString();
+                }
+            }
+
+            
+            using (SqlConnection myConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["localdbConnectionString1"].ConnectionString))
             {
                 String encrypted_post;
                 String plain_post;
+                DataTable dt = new DataTable();
+                String query = "SELECT * FROM [Post] WHERE [IsPostEntry] = @posted AND [Author_Email] = @email AND Permission_Status = @Permission";
 
                 SqlCommand myCommand = new SqlCommand(query, myConnection);
                 myConnection.Open();
                 myCommand.CommandType = CommandType.Text;
-                myCommand.Parameters.AddWithValue("@Permission", permission);
+                myCommand.Parameters.AddWithValue("@Permission", "Public");
+                myCommand.Parameters.AddWithValue("@email", FriendEmail);
+                myCommand.Parameters.AddWithValue("@posted", 1);
                 SqlDataAdapter da = new SqlDataAdapter(myCommand);
                 da.Fill(dt);
 
